b1c5f43859919108d68b4957022c87bdafd15003
snickbit <nicklowe@snickbit.com>
2022-05-24 17:37:20 -0400

    feat: add SSeperator component

---
+++ packages/quasar-components/src/components/SSeperator.vue
@@ -0,0 +1,49 @@
+<template>
+	<div :class="classes" class="s-separator">
+		<slot>
+			<q-separator :inset="inset" :vertical="vertical"/>
+		</slot>
+	</div>
+</template>
+<script lang="ts" setup>
+import {ref, watch} from 'vue'
+
+interface Props {
+	vertical?: boolean
+	inset?: boolean
+	spaced?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'none'
+}
+
+const {vertical, inset = true, spaced = 'xl'} = defineProps<Props>()
+
+const classes = ref<string[]>([])
+
+watch(() => spaced, makeClasses)
+
+makeClasses()
+
+function makeClasses() {
+	const cls: string[] = []
+
+	const direction = vertical ? 'px' : 'py'
+
+	if (vertical) {
+		cls.push('s-separator--vertical')
+	}
+
+	if (spaced !== 'none') {
+		cls.push(`q-${direction}-${spaced}`)
+	}
+
+	classes.value = cls
+}
+</script>
+<style lang="scss" scoped>
+.s-separator--vertical {
+	height: 100%;
+
+	.q-separator--vertical {
+		height: 100%;
+	}
+}
+</style>
