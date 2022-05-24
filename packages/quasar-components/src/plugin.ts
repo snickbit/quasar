afe95f67709a9e71a36efe15a17e9d4dd4eb08b8
snickbit <nicklowe@snickbit.com>
2022-05-24 16:00:50 -0400

    build: overhaul build to allow for full installation

---
+++ packages/quasar-components/src/plugin.ts
@@ -0,0 +1,43 @@
+import {App} from 'vue'
+import packageJson from '../package.json'
+
+type ComponentType = any
+
+export interface PluginInstance {
+	version: string
+	install: (app: App) => void
+}
+
+export default function (components?: ComponentType[]): PluginInstance {
+	const installTargets: App[] = []
+
+	function registerComponent(app: App, name: string, component: ComponentType): void {
+
+		const registered = app.component(name)
+
+		if (!registered) {
+			app.component(name, component)
+		}
+	}
+
+	function install(app: App): void {
+		if (installTargets.includes(app)) return
+
+		installTargets.push(app)
+
+		components.forEach((component) => {
+			const {name, alias} = component
+			registerComponent(app, name, component)
+			if (alias) {
+				alias.forEach((aliasName: string) => {
+					registerComponent(app, aliasName, component)
+				})
+			}
+		})
+	}
+
+	return {
+		version: packageJson.version,
+		install
+	}
+}
