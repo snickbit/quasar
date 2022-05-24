aacded99f26ef392d2f5e6d819ad5da56a69a607
snickbit <nicklowe@snickbit.com>
2022-05-24 16:01:23 -0400

    feat: add SPage layout

---
+++ packages/quasar-components/src/layouts/SPage.vue
@@ -0,0 +1,32 @@
+<template>
+	<q-page :padding="padding" :style-fn="styleFn" :class="pageClass">
+		<div class="q-page--inner" :class="innerClass">
+			<slot/>
+		</div>
+	</q-page>
+</template>
+<script lang="ts" setup>
+import {ref, watch} from 'vue'
+
+interface Props {
+	padding: boolean
+	styleFn: (offset: number, height: number) => any
+	center: boolean
+	innerClass: string[] | Record<string, any>
+}
+
+const {padding = false, styleFn, center = false, innerClass = []} = defineProps<Props>()
+
+const pageClass = ref<string[]>([])
+
+watch(() => center, makeClasses)
+
+makeClasses()
+
+function makeClasses() {
+	if (center) {
+		pageClass.value = ['row items-center justify-evenly']
+	}
+}
+</script>
+<style lang="scss" scoped></style>
