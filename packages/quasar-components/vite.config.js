75c6b3ae06d57eb21a0950233d46eaa06a342588
Snickbit <snickbit@users.noreply.github.com>
2022-06-23 14:31:31 -0400

    build: add source maps

--- packages/quasar-components/vite.config.js
+++ packages/quasar-components/vite.config.js
@@ -7,6 +7,7 @@ import dts from 'vite-dts'
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
+		sourcemap: true,
 		lib: {
 			entry: path.resolve(__dirname, 'src/index.ts'),
 			name: 'snickbit-quasar',
